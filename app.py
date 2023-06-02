ee8b58b3375fca383a0f6fa95f79325e327ebaf5
Dr. Ernesto Lee <socrates73@gmail.com>
2023-06-02 09:58:42 -0700

    Update app.py

--- app.py
+++ app.py
@@ -2,3 +2,5 @@ import streamlit as st
 st.title('My CE App') # Sets the the title of your page
 st.header('Data Visualization Section')  # Sets a header for a section
 st.subheader('Subsection: Pie Chart Analysis')  # Sets a subheader for a subsection
+st.text('This section focuses on data preprocessing.')
+st.markdown('**This is some bold text**')
