88641f03ff5321e742fb212286e3354bf57732c5
Dr. Ernesto Lee <socrates73@gmail.com>
2023-06-02 10:03:13 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+# https://docs.streamlit.io/library/cheatsheet
 import streamlit as st
 st.title('My CE App') # Sets the the title of your page
 st.header('Data Visualization Section')  # Sets a header for a section
@@ -13,3 +14,10 @@ data = {
 df = pd.DataFrame(data)
 
 st.write(df)
+
+age = st.slider('Your age', min_value=0, max_value=100)  # Returns the value selected by the user
+if st.button('Say Hello'):  # Returns True if the user clicks the button
+    st.write('Hello!')
+show_df = st.checkbox('Show DataFrame')  # Returns True if the user checks the box, False otherwise
+if show_df:
+    st.write(df)
