c596a3cd8cf4abe984b614da5fb56f1961fc4cdd
Dr. Ernesto Lee <socrates73@gmail.com>
2023-06-02 10:00:51 -0700

    Update app.py

--- app.py
+++ app.py
@@ -4,3 +4,12 @@ st.header('Data Visualization Section')  # Sets a header for a section
 st.subheader('Subsection: Pie Chart Analysis')  # Sets a subheader for a subsection
 st.text('This section focuses on data preprocessing.')
 st.markdown('**This is some bold text**')
+import pandas as pd
+
+data = {
+    'A': [45, 37, 89, 67],
+    'B': ['John', 'Max', 'Lisa', 'Chris']
+}
+df = pd.DataFrame(data)
+
+st.write(df)
