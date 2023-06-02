f1e37908aa80ce1559cb5cd93af90ada9532ab23
Dr. Ernesto Lee <socrates73@gmail.com>
2023-06-02 10:36:50 -0700

    Update app.py

--- app.py
+++ app.py
@@ -45,6 +45,38 @@ df['Adult'] = df['ages'].apply(lambda x: 'Yes' if x >= 18 else 'No')
 
 # Display DataFrame in Streamlit
 st.dataframe(df)
+import pandas as pd
+from sklearn import datasets
+import matplotlib.pyplot as plt
+import streamlit as st
+
+# Load the iris dataset
+iris = datasets.load_iris()
+df = pd.DataFrame(data=iris.data, columns=iris.feature_names)
+
+# Display the DataFrame in Streamlit
+st.dataframe(df)
+
+# Show general information about the dataset
+st.text(df.info())
+
+# Show statistical information about the dataset
+st.write(df.describe())
+
+# Select a feature to display histogram
+feature = st.selectbox('Select a feature', df.columns)
+
+# Plot histogram
+fig, ax = plt.subplots()
+ax.hist(df[feature], bins=20)
+
+# Set the title and labels
+ax.set_title(f'Histogram of {feature}')
+ax.set_xlabel(feature)
+ax.set_ylabel('Frequency')
+
+# Display the plot
+st.pyplot(fig)
 
 
 import pandas as pd
